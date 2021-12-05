30aec3b614c052fc7e3eee9e4c5111da4ecb11dd
Michael Ross <58342654+MichaelTRoss0@users.noreply.github.com>
2021-12-05 01:18:20 -0500

    Reorganized the class name package scheming. The @Mod annotation is now being used correctly.

--- src/main/java/com/michaetlross/packofcharcoal/CharcoalPack.java
+++
@@ -1,8 +0,0 @@
-package com.michaetlross.packofcharcoal;
-
-import net.minecraftforge.fml.common.Mod;
-
-@Mod(name="Pack of Charcoal", value = null)
-public class CharcoalPack {
-	
-}
---
+++ src/main/java/com/michaetlross/packofcharcoal/item/PackOfCharcoalItem.java
@@ -0,0 +1,8 @@
+package com.michaetlross.packofcharcoal.item;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod("packofcharcoal")
+public class PackOfCharcoalItem {
+	
+}
